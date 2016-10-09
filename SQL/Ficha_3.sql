cdf7bd1d4132a7945cda4dfd5c2fa9e8af8574b2
costa <costa@costa-N53SN>
2016-10-09 16:06:59 +0100

    Ficha 3 SQL

---
+++ SQL/Ficha_3.sql
@@ -0,0 +1,8 @@
+FX3_EX1
+select titulo
+from livros
+where to_char(data_edicao,'yyyy') = 2011;
+
+FX3_EX2
+select  to_char(sysdate, 'HH:MI:SS') "Hora Actual", to_char(sysdate, 'DD, Month,YYYY')
+from dual;
