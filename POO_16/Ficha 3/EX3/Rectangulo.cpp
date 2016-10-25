1d31e7828d851ba45ce171589d414edfbf025abe
Jorge Costa <Jorgemdcosta@gmail.com>
2016-10-25 14:32:08 +0100

    Update Rectangulo.cpp

--- POO_16/Ficha 3/EX3/Rectangulo.cpp
+++ POO_16/Ficha 3/EX3/Rectangulo.cpp
@@ -1,24 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Rectangulo.cpp
- * Author: costa
- * 
- * Created on 19 de Outubro de 2016, 23:37
- */
+#include <iostream>
+#include <sstream>
+using namespace std;
 
 #include "Rectangulo.h"
 
-Rectangulo::Rectangulo() {
+
+Rectangulo::Rectangulo( int x, int y, int larg, int alt)
+:canto(x, y)
+{
+	if(!setLargura(larg)){
+		largura = 1;
+	}
+	if(!setAltura(alt)){
+		altura = 1;
+	}
+	cout << "\nRectangulo(...)  " << getAsString();
 }
 
-Rectangulo::Rectangulo(const Rectangulo& orig) {
+
+Rectangulo::~Rectangulo(){
+	 cout << "\n~Rectangulo()  " << getAsString();
 }
 
-Rectangulo::~Rectangulo() {
+Ponto Rectangulo::getCanto()const{
+	return canto;
+}
+int Rectangulo::getLargura()const{
+	return largura;
+}
+int Rectangulo::getAltura()const{
+	return altura;
+}
+std::string Rectangulo::getAsString()const{
+	ostringstream oss;
+	oss << " Rectangulo " << canto.getAsString() << " larg: " 
+		<< largura << " alt: " << altura;
+	return oss.str();
 }
 
+int Rectangulo::calculaArea()const{
+	return largura * altura;
+}
+
+void Rectangulo::setCanto( const Ponto & c){
+	canto = c;
+}
+bool Rectangulo::setLargura( int larg){
+	if(larg <=  0){
+		return false;
+	}
+	largura = larg;
+	return true;
+}
+bool Rectangulo::setAltura( int alt){
+	if(alt <=  0){
+		return false;
+	}
+	altura = alt;
+	return true;
+}
