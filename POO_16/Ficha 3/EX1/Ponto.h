bf8a073807cf74187c46259293e7bfbf4e887419
Jorge Costa <Jorgemdcosta@gmail.com>
2016-10-25 14:33:45 +0100

    Update Ponto.h

--- POO_16/Ficha 3/EX1/Ponto.h
+++ POO_16/Ficha 3/EX1/Ponto.h
@@ -1,50 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Ponto.h
- * Author: costa
- *
- * Created on 19 de Outubro de 2016, 22:37
- */
-
 #ifndef PONTO_H
 #define PONTO_H
-#include <string>
 
+#include <string>
+#include <iostream>
 using namespace std;
 
-class Ponto {
-    int x, y;
+class Ponto{
+	int x, y; // coordenadas
 public:
-    Ponto(int x0, int y0);
-    
-    
-    Ponto(const Ponto& orig);
-    virtual ~Ponto();
-    
-    
-    
-//    int getX() const;
-//    int getY() const;
-//    
-//    void setX(int x0);
-//    void setY(int y0);
-    
-    string getAsString() const;
-    
-    
-    double calculaDistancia(const Ponto & outro) const;
-    void setY(int y);
-    int getY() const;
-    void setX(int x);
-    int getX() const;
-    
+	// nao deve ser possivel construir objectos desta classe sem a indicacao das suas coordenadas 
+//	Ponto( int x0, int y0);  
+        
+	// ser possivel criar pontos sem especificacao das suas coordenadas ( ficam na posicao (0,0))
+	Ponto( int x0 = 0, int y0 = 0); 
+//	Ponto( const Ponto & z);
+	// so para testes
+	~Ponto();
 
+	// as funcoes que permitem obter os dados devem poder ser chamadas sobre objectos Ponto constantes
+	int getX()const;
+	int getY()const;
+
+	// as que modificam as coordenadas nao devem poder ser chamadas sobre objectos Ponto constantes
+	void setX(int x0);
+	void setY(int y0);
+
+	// obter um objecto string com a descricao textual do seu conteudo
+	string getAsString()const;
+
+	// obter a distancia entre dois pontos
+	double calculaDistancia(const Ponto & outro)const;
 };
 
-#endif /* PONTO_H */
+#endif
+
+
 
