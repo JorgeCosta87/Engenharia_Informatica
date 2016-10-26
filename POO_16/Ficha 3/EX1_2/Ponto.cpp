2524ac99c94881059e79441edb25ce507b31d4a3
costa <costa@costa-N53SN>
2016-10-26 22:12:37 +0100

    update

---
+++ POO_16/Ficha 3/EX1_2/Ponto.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+#include "Ponto.h"
+
+
+
+Ponto::Ponto(int x0, int y0):x(x0), y(y0){
+//	x = x0;
+//	y = y0;
+	cout << "\nPonto(int x0, int y0)  " << getAsString();
+}
+//Ponto::Ponto( const Ponto & z){
+//		x = z.x;
+//		y = z.y;
+//		std::cout << "\nPonto(const Ponto & z)  " << getAsString();
+//}
+Ponto::~Ponto(){
+	cout << "\n~Ponto()  " << getAsString();
+}
+
+int Ponto::getX()const{
+	return x;
+}
+int Ponto::getY()const{
+	return y;
+}
+
+void Ponto::setX(int x0){
+	x = x0;
+}
+void Ponto::setY(int y0){
+	y = y0;
+}
+
+// obter um objecto string com a descricao textual do seu conteudo (formato  (x  / y) ).
+string Ponto::getAsString()const{
+	ostringstream oss;
+	oss << " ( " << x << "," << y << ") ";
+	return oss.str();
+}
+
+double Ponto::calculaDistancia(const Ponto & outro)const{
+	//  x, y, coordenadas do ponto que invoca (apontado por this)
+	// outro.x, outro.y coordenadas do ponto recebido como parametro 
+	double d2 = (x - outro.x)*(x - outro.x) + (y - outro.y)*(y - outro.y);
+	return sqrt(d2);
+}
