21caf72b823bb53118a45176a0674b094989a007
costa <costa@costa-N53SN>
2016-11-18 20:58:48 +0000

    update

---
+++ POO_16/Ficha 5/EX3/ABC.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ABC.cpp
+ * Author: costa
+ * 
+ * Created on 16 de Novembro de 2016, 22:50
+ */
+
+#include "ABC.h"
+
+ABC::ABC() {
+}
+
+ABC::ABC(const ABC& orig) {
+}
+
+ABC::~ABC() {
+}
+
---
+++ POO_16/Ficha 5/EX3/ABC.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ABC.h
+ * Author: costa
+ *
+ * Created on 16 de Novembro de 2016, 22:50
+ */
+
+#ifndef ABC_H
+#define ABC_H
+
+class ABC {
+public:
+    ABC();
+    ABC(const ABC& orig);
+    virtual ~ABC();
+private:
+
+};
+
+#endif /* ABC_H */
+
