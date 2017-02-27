4001e88185e79aadcfe44a5ce69f0a464795db78
JorgeCosta87 <jorgemdcosta@gmail.com>
2017-02-27 02:55:45 +0000

    update SO2

--- SO2/ex_1/ex_1/ex3_proc.c
+++ SO2/ex_1/ex_1/ex3_proc.c
@@ -7,28 +7,39 @@
 
 //Pág 34 a 36 wsp
 
+#define MAX 256
 
 
-int _tmain(int argc, TCHAR ** argv)
+int _tmain(int argc, LPTSTR  argv[])
 {
-	TCHAR str[50], cmdLine[100];
+	TCHAR c, cmdLine[MAX];
 
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
 
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(STARTUPINFO);
 
 #ifdef UNICODE
-
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
-
 #endif // 
 
-	CreateProcess(NULL, argv[0], NULL, NULL, 0, 0, NULL, NULL, &si, &pi);
 
+	_tprintf(TEXT("Launch a new application? (Y/N)"));
+	_tscanf_s(TEXT("%c"), &c, 1);
+
+	if (c == 'Y' || c == 'y')
+	{
+		ZeroMemory(&si, sizeof(STARTUPINFO));
+		si.cb = sizeof(STARTUPINFO);
+		GetModuleFileName(NULL, cmdLine, MAX);
+
+		_tprintf(TEXT("Process launch: %s\n"), cmdLine);
+
+		if (CreateProcess(NULL, cmdLine, NULL, NULL, 0, 0, NULL, NULL, &si, &pi))
+			_tprintf(TEXT("Sucess\n"));
+		else
+			_tprintf(TEXT("Error\n"));
+	}
 
 	return 0;
-
 }
